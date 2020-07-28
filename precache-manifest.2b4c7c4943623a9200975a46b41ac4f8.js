6742bfd548c5ee797d726cc45bee85b40a33f020
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-07-28 10:16:31 +0100

    Updates

---
+++ precache-manifest.2b4c7c4943623a9200975a46b41ac4f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8e7bc7eceb34b690cc3aebed82ef8d",
+    "url": "/Cake-WebPage/index.html"
+  },
+  {
+    "revision": "28a3002c0b8e8c8fd74d",
+    "url": "/Cake-WebPage/static/css/main.db7fcf25.chunk.css"
+  },
+  {
+    "revision": "c7b00b6c2bb98a2f3456",
+    "url": "/Cake-WebPage/static/js/2.58aa19ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cake-WebPage/static/js/2.58aa19ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a3002c0b8e8c8fd74d",
+    "url": "/Cake-WebPage/static/js/main.aea2ce32.chunk.js"
+  },
+  {
+    "revision": "ce652984898f9d829dce",
+    "url": "/Cake-WebPage/static/js/runtime-main.594ce6f6.js"
+  }
+]);
